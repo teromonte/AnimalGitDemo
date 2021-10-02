86ec316c399b09b34129a8eafb1e8c1039105408
Thiago <tar.monteiro@campus.fct.unl.pt>
2021-10-02 21:55:55 +0100

    Initial version of our main class.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Miguel Goulao, n. xxxxx
+ * Demo for the Git lab on Software Engineering
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Animal git demo");
+    }
+}
