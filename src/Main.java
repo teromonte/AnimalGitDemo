74731e4033de731c39531e392a6f0886cfdb3e41
Thiago <tar.monteiro@campus.fct.unl.pt>
2021-10-02 22:42:40 +0100

    Bobby and kitty

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Super Animal git demo");
+        System.out.println("Let's hear it from the pets");
+        Animal dog = new Dog("Bobby");
+        Animal cat = new Cat("Kitty");
+        System.out.println(dog.getName() + " says " + dog.speak());
+        System.out.println(cat.getName() + " says " + cat.speak());
     }
 }
